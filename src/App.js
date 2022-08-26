07ade7c85a3bb8e6307d3957d44d0d201f72cca7
Anova Hou <anova@Anovas-MacBook-Air.local>
2022-08-26 15:24:22 -0700

    configure react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+// import logo from './rose.gif';
+import rose from './rose.gif';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={rose} className="Rose" alt="a blooming rose <3" />
+
+        <p className="H1">
+          a place for ideas
         </p>
-        <a
+
+        <p className="Paragraph">
+        at times, i wonder: what will remain?
+        </p>
+
+        <p className="Paragraph">
+        of the internet, of the cyberspace
+        </p>
+
+      <p className="Paragraph">
+        of you,
+        </p>
+
+        <p className="Paragraph">
+        of me?
+        </p>
+
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+
+      <header className="Section1">
+
+        <h1>
+          hey
+        </h1>
+
+      </header>
+
+
     </div>
+
   );
 }
 
